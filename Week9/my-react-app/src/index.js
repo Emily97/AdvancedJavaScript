71e0388256aa95115f3a62eee988f58647c07839
Emily Meagher <N00150623@iadt.ie>
2018-12-05 16:38:03 +0000

    update index.js

--- Week9/my-react-app/src/index.js
+++ Week9/my-react-app/src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const users = [
+  {name: 'John', age: 32},
+  {name: 'Mary', age: 22},
+  {name: 'Sue', age: 44}
+];
+
+// User Component
+function User(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p style = {{'color': 'red'}}>{props.age}</p>
+    </div>
+  );
+}
+
+const userList = users.map( u => <User name={u.name} age = {u.age} />);
+
 ReactDOM.render(
-  <div><h1>Hello World!</h1><h1>Goodbye</h1></div>,
-  document.getElementById('mountPoint')
+  <div>{userList}</div>,
+  document.getElementById('root')
 );
