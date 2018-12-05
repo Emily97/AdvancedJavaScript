90b6f299477b55b1b1258c6c46338bb0ab3dd421
Emily Meagher <N00150623@iadt.ie>
2018-12-05 15:35:49 +0000

    update of index html and js

--- Week9/my-react-app/src/index.js
+++ Week9/my-react-app/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1>Hello World!</h1>,
-  document.getElementById('root')
+  <div><h1>Hello World!</h1><h1>Goodbye</h1></div>,
+  document.getElementById('mountPoint')
 );
