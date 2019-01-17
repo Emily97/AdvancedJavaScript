826fb1e36d44e420a2603007360a70c8812c58d8
Emily Meagher <N00150623@iadt.ie>
2019-01-17 16:37:54 +0000

    upload progress

--- Week12/my-react-app/src/App.js
+++ Week12/my-react-app/src/App.js
@@ -1,27 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
+import MovieRow from './MovieRow.js';
+// import $ from 'jquery';
+import axios from "axios";
 
 class App extends Component {
     constructor(props) {
         super(props);
-        console.log("This is my initializer");
+        this.state = { movie: []};
+        this.performSearch("marvel");
+}
+  performSearch(searchTerm) {
+    console.log("Perform search using movie database");
 
+    axios
+      .get("https://api.themoviedb.org/3/search/movie?api_key=2e73354b8604079af779d97230848993&query=" + searchTerm)
+      .then(response => {
+        // this.setState({movies: response.data});
+        console.log("Success");
+        console.log(response.data);
 
-        const movies = [
-            {id: 0, title:"Avengers: Infinity War", overview: "everyone dies"},
-            {id: 1, title:"Avengers", overview: "new york attack"},
-        ]
-        this.state = {rows:(
-            <p>My row</p>
-        )}
-        var movieRows =[]
-        movies.forEach((movie) => {
-            console.log(movie.title)
+        var movieRows = [];
+
+        const results = response.data.results;
+        results.forEach((movie) =>{
+          // console.log(movie.title);
+          movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path;
+          console.log(movie.poster_path);
+          const movieRow = <MovieRow key={movie.id} movie={movie}/>
+          movieRows.push(movieRow);
         })
-    }
+
+        this.setState({rows: movieRows});
+      })
+      .catch(err => {
+        // GET failed, log the error
+        console.log(err);
+      });
+  }
+
+
+  searchChangeHandler(event) {
+    console.log(event.target.value);
+    const boundObject = this;
+    const searchTerm = event.target.value;
+    boundObject.performSearch(searchTerm)
+  }
+
   render() {
     return (
-      <div className="App">
+      <div>
         <table className="titlebar">
             <tbody>
                 <tr>
@@ -39,11 +67,10 @@ class App extends Component {
         <input style={{
             fontSize: 24,
             display: 'block',
-            width: '99%',
             paddingTop:8,
             paddingBottom:8,
             paddingLeft: 16
-        }} placeholder="Search" />
+        }} onChange={this.searchChangeHandler.bind(this)} placeholder="Search" />
 
         {this.state.rows}
       </div>
