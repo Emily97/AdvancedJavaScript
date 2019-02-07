3180cad50b7c3ac8f0a46c3e378a7884ca297e74
Emily97 <N00150623@student.iadt.ie>
2019-02-07 21:46:54 +0000

    update

--- Movie_CA/routing/src/components/elements/PanelLayout/PanelLayout.js
+++ Movie_CA/routing/src/components/elements/PanelLayout/PanelLayout.js
@@ -1,31 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './PanelLayout.css';
 
-const PanelLayout = ({ header, loading, children }) => {
+const PanelLayout = (props) => {
 
   const renderElements = () => {
-    const gridElements = children.map( (element, i) => (
-      <div key={i} className="movie-grid-element">
-        {element}
-      </div>
-    ))
+    const gridElements = props.children.map( (element, i) => {
+        return(
+          <div key={i} className="movie-grid-element">
+            {element}
+          </div>
+        )
+    })
     return gridElements;
   }
 
   return (
     <div className="movie-grid">
-      {header && !loading ? <h1>{header}</h1> : null}
+      {props.header && !props.loading ? <h1>{props.header}</h1> : null}
       <div className="movie-grid-content">
          {renderElements()}
       </div>
     </div>
   )
-}
-
-PanelLayout.propTypes = {
-  header: PropTypes.string,
-  loading: PropTypes.bool
 }
 
 export default PanelLayout;
